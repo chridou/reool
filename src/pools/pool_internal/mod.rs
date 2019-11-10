6e07fee6e820e200eb9300f535d0ff5e2d9fb7e5
panicbit <panicbit.dev@gmail.com>
2019-11-10 01:33:03 +0100

    futurize pool_internal/mod.rs

--- src/pools/pool_internal/mod.rs
+++ src/pools/pool_internal/mod.rs
@@ -4,13 +4,12 @@ use std::sync::{Arc, Weak};
 use std::time::{Duration, Instant};
 
 use futures::{
-    future::{self, Future, Loop},
-    stream::Stream,
-    sync::mpsc,
-    Poll,
+    future::{Future, FutureExt},
+    stream::{StreamExt, TryStreamExt},
+    channel::mpsc,
 };
 use log::{debug, trace, warn};
-use tokio::{self, timer::Delay};
+use tokio::{self, timer::delay};
 
 use crate::activation_order::ActivationOrder;
 use crate::backoff_strategy::BackoffStrategy;
@@ -127,7 +126,7 @@ where
         )
     }
 
-    pub fn check_out(&self, timeout: Option<Duration>) -> CheckoutManaged<T> {
+    pub fn check_out(&self, timeout: Option<Duration>) -> impl Future<Output = Result<Managed<T>, CheckoutError>> + '_ {
         self.inner_pool.check_out(timeout)
     }
 
@@ -148,7 +147,7 @@ where
 }
 
 impl PoolInternal<ConnectionFlavour> {
-    pub fn ping(&self, timeout: Duration) -> impl Future<Item = Ping, Error = ()> + Send {
+    pub fn ping(&self, timeout: Duration) -> impl Future<Output = Result<Ping, ()>> + '_ {
         self.inner_pool.ping(timeout)
     }
 }
@@ -166,38 +165,41 @@ fn start_new_conn_stream<T, C>(
     let spawn_handle = executor.spawn_unbounded(receiver);
 
     let mut is_shut_down = false;
-    let fut = spawn_handle.for_each(move |msg| {
+    let fut = spawn_handle.map(Ok).try_for_each(move |msg| async {
         if is_shut_down {
             trace!("new connection requested on finished stream");
-            Box::new(future::err(()))
-        } else {
-            match msg {
-                NewConnMessage::RequestNewConn => {
-                    if let Some(existing_inner_pool) = inner_pool.upgrade() {
-                        trace!("creating new connection");
-                        let fut = create_new_managed(
+            return Err(());
+        }
+
+        match msg {
+            NewConnMessage::RequestNewConn => {
+                if let Some(existing_inner_pool) = inner_pool.upgrade() {
+                    trace!("creating new connection");
+
+                    let managed =
+                        create_new_managed(
                             Instant::now(),
                             connection_factory.clone(),
                             Arc::downgrade(&existing_inner_pool),
                             back_off_strategy,
                         )
-                        .map(|_| ())
-                        .map_err(|err| warn!("Failed to create new connection: {}", err));
-                        drop(existing_inner_pool);
-                        Box::new(fut)
-                    } else {
-                        warn!("attempt to create new connection even though the pool is gone");
-                        Box::new(future::err(())) as Box<dyn Future<Item = _, Error = _> + Send>
-                    }
-                }
-                NewConnMessage::Shutdown => {
-                    debug!("shutdown new conn stream");
-                    is_shut_down = true;
-                    Box::new(future::err(()))
+                        .await
+                        .map_err(|_| ())?;
+
+                    Ok(())
+                } else {
+                    warn!("attempt to create new connection even though the pool is gone");
+                    Err(())
                 }
             }
+            NewConnMessage::Shutdown => {
+                debug!("shutdown new conn stream");
+                is_shut_down = true;
+                Err(())
+            }
         }
-    });
+    })
+    .map(|_| ());
 
     executor.execute(fut).unwrap()
 }
@@ -210,94 +212,64 @@ impl<T: Poolable> Clone for PoolInternal<T> {
     }
 }
 
-fn create_new_managed<T: Poolable, C>(
+async fn create_new_managed<T: Poolable, C>(
     initiated_at: Instant,
     connection_factory: Arc<C>,
     weak_inner_pool: Weak<InnerPool<T>>,
     back_off_strategy: BackoffStrategy,
-) -> NewManaged<T>
+) -> Result<Managed<T>, NewConnectionError>
 where
     T: Poolable,
     C: ConnectionFactory<Connection = T> + Send + Sync + 'static,
 {
-    let fut = future::loop_fn((weak_inner_pool, 1), move |(weak_inner, attempt)| {
-        if let Some(inner_pool) = weak_inner.upgrade() {
-            let start_connect = Instant::now();
-            let fut = connection_factory
-                .create_connection()
-                .then(move |res| match res {
-                    Ok(conn) => {
-                        trace!("new connection created");
-                        inner_pool.notify_connection_created(
-                            initiated_at.elapsed(),
-                            start_connect.elapsed(),
-                        );
-                        Box::new(future::ok(Loop::Break(Managed::fresh(
-                            conn,
-                            Arc::downgrade(&inner_pool),
-                        ))))
-                    }
-                    Err(err) => {
-                        inner_pool.notify_connection_factory_failed();
-                        if let Some(backoff) = back_off_strategy.get_next_backoff(attempt) {
-                            let delay = Delay::new(Instant::now() + backoff);
-                            warn!(
-                            "Attempt {} to create a connection failed. Retry in {:?}. Error: {}",
-                            attempt, backoff, err
-                        );
-                            Box::new(
-                                delay
-                                    .map_err(|err| NewConnectionError::new(Box::new(err)))
-                                    .and_then(move |()| {
-                                        future::ok(Loop::Continue((
-                                            Arc::downgrade(&inner_pool),
-                                            attempt + 1,
-                                        )))
-                                    }),
-                            )
-                                as Box<dyn Future<Item = _, Error = _> + Send>
-                        } else {
-                            warn!(
-                        "Attempt {} to create a connection failed. Retry immediately. Error: {}",
-                        attempt, err);
-                            Box::new(future::ok(Loop::Continue((
-                                Arc::downgrade(&inner_pool),
-                                attempt + 1,
-                            ))))
-                        }
-                    }
-                });
-            Box::new(fut) as Box<dyn Future<Item = _, Error = _> + Send>
-        } else {
-            Box::new(future::err(NewConnectionError::new(Box::new(
+    let mut attempt = 1;
+
+    loop {
+        let inner_pool = weak_inner_pool.upgrade()
+            .ok_or_else(|| NewConnectionError::new(Box::new(
                 PoolIsGoneError,
-            ))))
+            )))?;
+
+        let start_connect = Instant::now();
+
+        match connection_factory.create_connection().await {
+            Ok(conn) => {
+                trace!("new connection created");
+
+                inner_pool.notify_connection_created(
+                    initiated_at.elapsed(),
+                    start_connect.elapsed(),
+                );
+
+                break Ok(Managed::fresh(
+                    conn,
+                    Arc::downgrade(&inner_pool),
+                ));
+            }
+            Err(err) => {
+                inner_pool.notify_connection_factory_failed();
+
+                if let Some(backoff) = back_off_strategy.get_next_backoff(attempt) {
+                    warn!(
+                        "Attempt {} to create a connection failed. Retry in {:?}. Error: {}",
+                        attempt, backoff, err
+                    );
+
+                    delay(Instant::now() + backoff).await;
+
+                    continue;
+                } else {
+                    warn!(
+                        "Attempt {} to create a connection failed. Retry immediately. Error: {}",
+                        attempt, err
+                    );
+
+                    attempt += 1;
+
+                    continue;
+                }
+            }
         }
-    });
-    NewManaged::new(fut)
-}
-
-pub(crate) struct CheckoutManaged<T: Poolable> {
-    inner: Box<dyn Future<Item = Managed<T>, Error = CheckoutError> + Send + 'static>,
-}
-
-impl<T: Poolable> CheckoutManaged<T> {
-    pub fn new<F>(fut: F) -> Self
-    where
-        F: Future<Item = Managed<T>, Error = CheckoutError> + Send + 'static,
-    {
-        Self {
-            inner: Box::new(fut),
-        }
-    }
-}
-
-impl<T: Poolable> Future for CheckoutManaged<T> {
-    type Item = Managed<T>;
-    type Error = CheckoutError;
-
-    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
-        self.inner.poll()
     }
 }
 
@@ -359,28 +331,6 @@ impl<T: Poolable> Drop for Managed<T> {
 pub(crate) enum NewConnMessage {
     RequestNewConn,
     Shutdown,
-}
-
-pub(crate) struct NewManaged<T: Poolable> {
-    inner: Box<dyn Future<Item = Managed<T>, Error = NewConnectionError> + Send + 'static>,
-}
-
-impl<T: Poolable> NewManaged<T> {
-    pub fn new<F>(f: F) -> Self
-    where
-        F: Future<Item = Managed<T>, Error = NewConnectionError> + Send + 'static,
-    {
-        Self { inner: Box::new(f) }
-    }
-}
-
-impl<T: Poolable> Future for NewManaged<T> {
-    type Item = Managed<T>;
-    type Error = NewConnectionError;
-
-    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
-        self.inner.poll()
-    }
 }
 
 #[derive(Debug)]
